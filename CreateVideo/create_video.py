53754c752135f7e9850a63486169375ca72b1a08
Galaxysm <38530543+Galaxysm@users.noreply.github.com>
2025-09-28 15:08:15 +0800

    参数设定整理

--- CreateVideo/create_video.py
+++ CreateVideo/create_video.py
@@ -13,13 +13,18 @@ def wma_to_mp4_ffmpeg_direct(wma_file, image_file, output_mp4):
     print(f"✅ 转换成功！输出文件：{output_mp4}")
 
 
-# 示例调用
-count = 1
-while count <= 33:
+# 参数设定
+count = 1           # 起始index
+max_count = 33      # 终止index
+wma_file = f"E:\\01_影视动漫\有声书\盗墓笔记8之大结局上(周建龙)[33回]\盗墓笔记8-大结局上"
+img_file = f"E:\douyin\img\\"
+output_mp4 = f"E:\douyin\大结局上"
+
+while count <= max_count:
     formatted_count = str(count).zfill(2)
     wma_to_mp4_ffmpeg_direct(
-        f"E:\\01_影视动漫\有声书\盗墓笔记8之大结局上(周建龙)[33回]\盗墓笔记8-大结局上{formatted_count}.wma",
-        f"E:\douyin\img\\{formatted_count}.png",
-        f"E:\douyin\大结局上{formatted_count}.mp4")
+        wma_file + f"{formatted_count}.wma",
+        img_file + f"{formatted_count}.png",
+        output_mp4 + f"{formatted_count}.mp4")
 
     count += 1
